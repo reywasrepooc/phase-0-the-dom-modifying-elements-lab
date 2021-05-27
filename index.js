3c198ce3922fb0f1228cdaa5718f8f0ccf7084c7
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-05-27 16:46:11 -0400

    completed addignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.querySelector('#main').remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "<h1> Cooper is the champion</h1>".toString()
+
